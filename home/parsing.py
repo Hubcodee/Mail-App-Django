14885d3ab6238ff45a3f4154df9ee67bea07ab41
Shivanshu Suryakar <jiminnpark007@gmail.com>
2021-05-08 01:02:35 +0530

    Attachment issue resolved, added gradient background to sections on mailbox,compose mail  issue resolved, csrf_token for attachment mails rectified

--- home/parsing.py
+++ home/parsing.py
@@ -60,16 +60,17 @@ def fetch_mails(mail_type,emailid,password):
                             <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
                             <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
                             <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-                            <div class='container'><b> This email contains attachments which could be harmful we recommed Downloading only if your trust the Sender!</b></div>
+                            <div class='container' style='margin-top:50px'><b> This email contains attachments which could be harmful we recommed Downloading only if your trust the Sender!</b></div>
                             <form action='download' method='post'>
-                            {% csrf_token %}
+                            {% {} %}
                             <input name='send_from' style='display:none' value={}>
                             <input name='email' style='display:none' value={}>
                             <input name='password' style='display:none' value={}>
                             <button type='submit' onclick='location.href("download")' class='btn btn-danger center'> Download Attachments</button>
                             </form>
-                            """.format(mail_from,emailid,password)
+                            """.format("csrf_token",mail_from,emailid,password)
                             
+                            # mail_content.replace("csrf_token","{% csrf_token %}")
                 else:
                     temp['content_type']='other'
                     mail_content = message.get_payload(decode=True)
