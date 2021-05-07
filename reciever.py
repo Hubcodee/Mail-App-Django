498924a1ef214a1ffd6b0779aeda3f357f124e2f
Shivanshu Suryakar <68246011+jiminnpark@users.noreply.github.com>
2021-05-07 09:02:29 +0530

    Downloading attachments
    Code for downloading attachments added with several parameters

--- reciever.py
+++ reciever.py
@@ -1,6 +1,9 @@
 import email
 import imaplib
 from decouple import config
+import os
+from imbox import Imbox # pip install imbox
+import traceback
 
 EMAIL = config('GMAIL_USER')
 PASSWORD = config('GMAIL_PASS')
@@ -46,3 +49,84 @@ def fetch_mails():
 
 
 fetch_mails()
+# ------------- functionality to download attachments -------------------
+
+
+# enable less secure apps on your google account
+# https://myaccount.google.com/lesssecureapps
+
+host = "imap.gmail.com"
+# Please use different mail if you are using our code !
+username = "testmailtesting17@gmail.com"
+password = 'Testmail#12345'
+
+def downloadattachments(hst,usrname,passkey,fromuser):
+    if os.path.exists("attachments"):
+        pass
+    else:
+        os.mkdir("attachments")
+    download_folder = os.getcwd()+"/attachments"
+
+    if not os.path.isdir(download_folder):
+        os.makedirs(download_folder, exist_ok=True)
+
+    mail = Imbox(host, username=username, password=password, ssl=True, ssl_context=None, starttls=False)
+    # currently it will receive message from user called shivanshusurya192@gmail.com if you want to change to specific user pass its name
+
+    messages = mail.messages(sent_from=fromuser) # defaults to inbox
+
+    for (uid, message) in messages:
+        # mail.mark_seen(uid) # optional, mark message as read
+
+        for idx, attachment in enumerate(message.attachments):
+            try:
+                att_fn = attachment.get('filename')
+                download_path = f"{download_folder}/{att_fn}"
+                print(download_path)
+                with open(download_path, "wb") as fp:
+                    fp.write(attachment.get('content').read())
+            except:
+                print(traceback.print_exc())
+
+    mail.logout()
+
+# call this using following 
+downloadattachments(host,username,password,"shivanshusurya192@gmail.com")
+print("Downloaded successfully")
+
+"""
+Available Message filters: 
+
+# Gets all messages from the inbox
+messages = mail.messages()
+
+# Unread messages
+messages = mail.messages(unread=True)
+
+# Flagged messages
+messages = mail.messages(flagged=True)
+
+# Un-flagged messages
+messages = mail.messages(unflagged=True)
+
+# Messages sent FROM
+messages = mail.messages(sent_from='sender@example.org')
+
+# Messages sent TO
+messages = mail.messages(sent_to='receiver@example.org')
+
+# Messages received before specific date
+messages = mail.messages(date__lt=datetime.date(2018, 7, 31))
+
+# Messages received after specific date
+messages = mail.messages(date__gt=datetime.date(2018, 7, 30))
+
+# Messages received on a specific date
+messages = mail.messages(date__on=datetime.date(2018, 7, 30))
+
+# Messages whose subjects contain a string
+messages = mail.messages(subject='Christmas')
+
+# Messages from a specific folder
+messages = mail.messages(folder='Social')
+"""
